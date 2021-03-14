db2548fb2343136c58a6ac1e983d20afbd87e31b
youssef-saidi <78275450+youssef-saidi@users.noreply.github.com>
2021-03-14 13:41:49 +0100

    Add files via upload

---
+++ fr.php
@@ -0,0 +1,75 @@
+<?php
+$lang=array(
+    // Home1.php
+    "navbar-item-1" =>"Accueil",
+    "navbar-item-2" =>"Services", 
+    "navbar-item-3" =>"A Propos",
+    "navbar-item-4" =>"Contact",
+    "choose-mus" =>"Choisissez la musique ",
+    "title-musi" =>"Musique",
+    "read-more"=>"Lire la suite",
+    "title-CAO"=>"CAO/DAO",
+    "text-CAO"=>"Conception Assisté par Ordinateur (les produit en 3D) / Dessin Assisté par Ordinateur (les plans industriels) : Cœur de métier de Nissed ...",
+    "title-I"=>"Sourcing Industriel",
+    "text-I"=>"S’appuyant du l’expérience en Tunisie de l’industriel français, la société Nissed est capable de ...",
+    "title-C-Q"=>"Sourcing Contrôle Qualité",
+    "text-C-Q"=>" Nous avons la possibilité de vous proposer un service de contrôle...",
+    "title-H"=>"Hydraulique",
+    "text-H"=>"Nous avons sélectionné des spécialistes en Tunisie ...",
+    "title-P"=>"Pneumatique",
+    "text-P"=>"Nous avons sélectionné des spécialistes en Tunisie ...",
+    "title-E"=>"Electricité",
+    "text-E"=>"Nous avons sélectionné des spécialistes en Tunisie ...",
+    "title-fao"=>"FAO",
+    "text-fao"=>"Fabrication Assisté par Ordinateur (la programmation pour les machines CNC) Nous avons sélectionné ... ",
+    "title-web"=>"Création des sites web, création des logo, chartes graphique et packaging ",
+    "text-web"=>" Nissed a sélectionné pour vous une équipe free lance de jeune designers  ... ",
+    "title-S-media"=>"Retrouvez-nous sur les réseaux sociaux ",
+    "footer-para"=>"Jeune start up tunisienne, née de la rencontre d’étudiants en école d’ingénieur, travaillants pour payer leurs études et d’un industriel français..",
+    "location"=>"Route de Ouardanine Sidi Ameur 5061 Sahlin Monastir Tunisie",
+    "title-w"=>"Sites web et Logo",
+    // service1.php
+"Para-C-A-D"=>"Conception Assisté par Ordinateur (les produit en 3D) / Dessin Assisté par Ordinateur (les plans industriels) : Cœur de métier de Nissed, l’équipe de départ de cette aventure est constituée de spécialistes de la CAO/DAO et bien que jeunes diplômés, ayant déjà une solide expérience industrielle en entreprise.",
+"Para-I"=>"S’appuyant du l’expérience en Tunisie de l’industriel français, la société Nissed est capable de vous trouver le sous-traitant adéquat à vos besoins, et de se charger pour vous de sa sélection et de son suivi",
+"Para-C-Q"=>"Nous avons la possibilité de vous proposer un service de contrôle qualité en interne mais également avec des partenaires qualifiés, pour vous assurer que la production de vos produits en Tunisie répondra à toutes vos exigences.",
+"Para-H"=>"Nous avons sélectionné des spécialistes en Tunisie afin de répondre a vos besoins en hydraulique",
+"Para-P"=>"Nous avons sélectionné des spécialistes en Tunisie afin de répondre a vos besoins en pneumatique",
+"Para-E"=>"Nous avons sélectionné des spécialistes en Tunisie afin de répondre a vos besoins en électricité",
+"Para-fao"=>"Fabrication Assisté par Ordinateur (la programmation pour les machines CNC) Nous avons sélectionné des spécialistes en Tunisie afin de répondre a vos besoins en FAO",
+"Para-web"=>"Nissed a sélectionné pour vous une équipe free lance de jeune designers et créateurs pour toute la partie marketing de votre entreprise et la création d’outils web, comme les sites, application ou autres",
+
+// about
+"title-society"=>"Notre société",
+"para-society"=>"Jeune start up tunisienne, née de la rencontre d’étudiants en école d’ingénieur, travaillants pour payer leurs études et d’un industriel français.
+L’idée est vite apparue qu’il était intéressant de proposer les services de jeunes diplômés tunisiens, ayant en plus une expérience dans l’industrie, mais également s’appuyer sur le vivier des écoles de techniciens et d’ingénieurs afin d’avoir les ressources pour répondre à toutes les problématiques.
+En interne, nous n’avons pas toutes les ressources pour répondre à toutes vos demandes, par exemple nous n’avons pas dans notre équipe un spécialiste pneumatique, mais nous avons fait pour vous le sourcing des meilleurs spécialistes sur le marché travaillant en free lance. Dans ce cas nous serons comme votre chef de projet externalisé en Tunisie.
+Nous travaillons en toute transparence avec nos clients, et vous saurez toujours quelle partie sera développée en interne chez nous, ou pilotée par nous, avec des sous traitants free lance.
+",
+
+"title-mem"=>"Membres",
+
+"para-m1"=>"Industriel français, globe trotter, ayant travaillé dans de nombreux pays et ayant plus de cinq ans d’expérience en Tunisie.
+Aujourd’hui à la tête d’une usine de métallurgie, mais ayant déjà travaillé dans la plasturgie, le médial ou l’industrie textile, son expérience lui permettra de comprendre et d’appréhender vos besoins
+Grace à son réseau d’entrepreneurs sur le sol tunisien, il est capable de vous trouver le sous-traitant qu’il vous faut et mettre une place l’équipe nécessaire au suivi de votre production.
+",
+"title-m1"=>"Laurent Baumgartner",
+"trav-m1"=>"Industriel français",
+
+"para-m2"=>"CEO de la société, jeune diplômée tunisienne, Malek Lassoued a déjà malgré son jeune âge une forte expérience industrielle et possède en Tunisie un réseau d'experts confirmés dans différents domaines.
+
+Elle est spécialiste en CAO et elle a déjà travaillé pour plusieurs entreprises européennes.
+Elle est parfaitement trilingue (arabe, français, anglais) et elle saura vous accompagner dans les projets que vous nous confierez
+",
+"title-m2"=>"Malek Lassoued",
+"trav-m2"=>"CEO de la société",
+
+
+
+"para-m3"=>"Responsable technique de la société, jeune diplômée tunisien, Aymen Tarchoun une personne autonome et parfaitement trilingue (arabe, français, anglais).
+Avec une expérience industrielle dans le département bureau d’étude et des fortes connaissances en outil d’informatique et technologique
+",
+"title-m3"=>"Aymen Tarchoun",
+"trav-m3"=>"Responsable technique de la société",
+);
+
+?>
---
+++ js.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){var count=0;var counter=setInterval(function(){if(count<101){$('.count').text(count+'%');$('.loader').css('width',count/6+'%');$('#body').css('display','none');count++;}else{$('#body').css('display','block');clearInterval(counter);}},80)})
+
+   
+
+     
+    
