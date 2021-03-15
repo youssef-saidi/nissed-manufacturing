1d45c8442446d1bb57020f71ded5bb49cc86b52e
youssef-saidi <78275450+youssef-saidi@users.noreply.github.com>
2021-03-15 16:35:17 +0100

    Add files via upload

---
+++ en.php
@@ -0,0 +1,78 @@
+<?php
+$lang=array(
+    "navbar-item-1" =>"Home",
+    "navbar-item-2" =>"Services", 
+    "navbar-item-3" =>"About Us",
+    "navbar-item-4" =>"Contact",
+    "choose-mus" =>"Play Video",
+    "title-musi" =>"Choose Music and Start",
+    "read-more"=>"Read more",
+    "title-CAO"=>"CAD/CAD",
+    "text-CAO"=>"CAD/CAD: Computer Assisted Design (3D products) / Computer Assisted Drawing (industrial plans): Nissed’s core business ...",
+    "title-I"=>"Industrial Sourcing",
+    "text-I"=>"Industrial Sourcing: Based on the experience of the French industrialist in Tunisia, Nissed is able to find you a suitable subcontractor ...",
+    
+    "title-C-Q"=>"Quality control sourcing",
+    "text-C-Q"=>"Quality control sourcing: We can offer you a quality control service in ...",
+    "title-H"=>"Hydraulic",
+    "text-H"=>"We have selected specialists in Tunisia ...",
+    "title-P"=>"Pneumatic",
+    "text-P"=>"We have selected specialists in Tunisia ...",
+    "title-E"=>"Electricity",
+    "text-E"=>"We have selected specialists in Tunisia ...",
+    "title-fao"=>"CAM",
+    "text-fao"=>" Computer Assisted Manufacturing (programming for CNC machines ... ",
+    "title-web"=>"Website creation, logo, graphic and packaging creation",
+    "text-web"=>" Nissed has selected for you a free-lance team of young designers and designers for the whole ... ",
+    "title-S-media"=>"Find Us On Social Media",
+    "footer-para"=>"Young Tunisian start-up, born from the meeting of students in engineering school, working to pay for their studies and a French industrialist..",
+"location"=>"Ouardanine road Sidi Ameur 5061 Sahlin Monastir Tunisia",
+"title-w"=>"website and Logo",
+
+    // service
+"Para-C-A-D"=>"Computer Assisted Design (3D products) / Computer Assisted Drawing (industrial plans): Nissed’s core business, the starting team for this adventure is made up of CAD/CAD specialists and young graduates, who already have a solid industrial experience in the company.",
+"Para-I"=>"Based on the experience of the French industrialist in Tunisia, Nissed is able to find you a suitable subcontractor for your needs, and to take care of its selection and its effective monitoring",
+"Para-C-Q"=>"We can offer you a quality control service in internal but also with qualified partners, to ensure that the production of your products in Tunisia will meet all your requirements.",
+"Para-H"=>"We have selected specialists in Tunisia to meet your hydraulic needs",
+"Para-P"=>"We have selected specialists in Tunisia to meet your pneumatic needs",
+"Para-E"=>"We have selected specialists in Tunisia to meet your electricity needs",
+"Para-fao"=>"Computer Assisted Manufacturing (programming for CNC machines) We have selected specialists in Tunisia to respond to your needs in CAM .",
+"Para-web"=>"Nissed has selected for you a free-lance team of young designers and designers for the whole marketing part of your company and the creation of web tools, such as websites, applications or others.",
+// about
+"title-society"=>"Our Company",
+"para-society"=>"Young Tunisian start-up, born from the meeting of students in engineering school, working to pay for their studies and a French industrialist.
+The idea quickly emerged that it was interesting to offer the services of young Tunisian graduates, having in addition an experience in the industry, but also to rely on the pool of schools of technicians and engineers in order to have the resources to answer all the problems.
+Internally, we do not have all the resources to meet all your demands, for example we do not have a pneumatic specialist in our team, but we have made for you the sourcing of the best specialists on the market working freelance. In this case we will be like your outsourced project manager in Tunisia.
+We work seamlessly with our clients, and you will always know which part will be developed internally with us, or driven by us, with free-lance subcontractor.",
+
+"title-mem"=>"Members",
+
+"para-m1"=>"French industrialist, globetrotter, have worked in many countries and more than five years of experience in Tunisia.
+Today at the head of a metallurgical plant, but already worked in plastics, medical or textile industry, his experience will allow him to understand and apprehend your needs
+Thanks to its network of contractors on Tunisian soil, he is able to find the subcontractor you need and to put together the necessary team to monitor your production.
+",
+"title-m1"=>"Laurent Baumgartner",
+"trav-m1"=>"French industrialist",
+
+"para-m2"=>"CEO of the company, young Tunisian graduate, Malek Lassoued has already despite his young age a strong industrial experience and has in Tunisia a network of experienced experts in various fields.
+She is a CAD specialist and has worked for several European companies.
+She is perfectly trilingual (Arabic, French, English) and she will know how to accompany you in the projects you entrust to us",
+"title-m2"=>"Malek Lassoued",
+"trav-m2"=>"CEO of the company",
+
+
+
+"para-m3"=>"Technical manager of the company, young Tunisian graduate, Aymen Tarchoun an autonomous and perfectly trilingual person (Arabic, French, English).
+With an industrial experience in the design office department and a strong knowledge of computer and technological tools.
+",
+"title-m3"=>"Aymen Tarchoun",
+"trav-m3"=>"Technical manager",
+// footer
+"title-location"=>"Location :",
+"title-phone"=>"Phone/WhatsApp :",
+"espace"=>"<br/><br/>",
+
+
+);
+
+?>
